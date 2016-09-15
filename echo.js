a0fdce516abbae3661511dc42d1f51809faecbbe
Eugene Brodsky <ebr@users.noreply.github.com>
2016-09-15 09:42:51 -0400

    wrap the echoed object into json

--- echo.js
+++ echo.js
@@ -6,9 +6,11 @@ app.use(bodyParser.text());
 app.listen(8080);
 
 app.post('/echo',function(req,res){
-  console.log({ 
-    headers: req.headers, 
-    body: req.body
+  console.log({
+    echo: {
+      headers: req.headers, 
+      body: req.body      
+    }
   });
   res.sendStatus(200);
 });
